b1dee8826f80bf5c29859de46eb93f9a74ed6bda
KaustubhDhoran <157009784+KaustubhDhoran@users.noreply.github.com>
2026-05-15 17:13:15 +0530

    Add files via upload

---
+++ Mail.java
@@ -0,0 +1,29 @@
+package gmail;
+
+public class Mail {
+    private String sender;
+    private String receiver;
+    private String subject;
+    private String body;
+
+    public Mail(String sender, String receiver, String subject, String body) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public void getMailInfo() {
+        System.out.println("**** Mail ****");
+        System.out.println("Receiver: " + receiver);
+        System.out.println("Sender: " + sender);
+        System.out.println("Subject: " + subject);
+        System.out.println("Body: " + body);
+    }
+
+    public String getReceiverMail() { return receiver; }
+    public void setReceiverMail(String newReceiverMail) { this.receiver = newReceiverMail; }
+    public void setSubject(String newSubject) { this.subject = newSubject; }
+    public void setBody(String newBody) { this.body = newBody; }
+    public String getSender() { return sender; }
+}
